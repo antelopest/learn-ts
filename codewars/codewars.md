8af52935248855728c1d6977623951224a5797be
antelopest <antelopest@gmail.com>
2025-09-14 14:36:15 +0700

    main add codewars.md

---
+++ codewars/codewars.md
@@ -0,0 +1,14 @@
+# CodeWars
+
+## Tasks
+
+Уровень сложности от 8 kyu до 1 kyu.
+
+* 8 kyu
+* 7 kyu
+* 6 kyu
+* 5 kyu
+* 4 kyu
+* 3 kyu
+* 2 kyu
+* 1 kyu
---
+++ codewars/tasks/8 kyu/removeStringSpaces.ts
@@ -0,0 +1,15 @@
+// export function noSpace(x: string): string {
+//     return x.replace(/\s/g, '');
+// }
+
+/*
+* or
+*
+* export function noSpace(x: string): string {
+*   return x.replaceAll(' ', '');
+* }
+* */
+
+export function noSpace(x: string): string {
+    return x.replaceAll(' ', '');
+}
